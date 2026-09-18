cd66c6a660b4b2eb402c4e19357416e018a2bf21
Vansh Gujjar <gujjarvansh320@gmail.com>
2026-09-18 23:34:01 +0530

    fix the url error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Points to your Express backend
+  baseURL: 'https://membrix-backend.onrender.com', // Points to your Express backend
   headers: {
     'Content-Type': 'application/json',
   },
